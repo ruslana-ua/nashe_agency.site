84107f69acc63b68b2d844dda836504279d6a6e7
Your Name <you@example.com>
2024-11-26 22:40:34 +0100

    new page

--- js/app.js
+++ js/app.js
@@ -10186,6 +10186,45 @@
                 };
             }
         }));
+        const circle = document.querySelector(".circle");
+        if (circle) {
+            const links = document.querySelectorAll(".nav-main__link");
+            const isPointerAvailable = window.matchMedia("(hover: hover)").matches;
+            if (isPointerAvailable) {
+                const editPosCursor = e => {
+                    const {clientX: x, clientY: y} = e;
+                    circle.style.left = x + "px";
+                    circle.style.top = y + "px";
+                };
+                const animateit = function(e) {
+                    const span = this.querySelector("span");
+                    const {offsetX: x, offsetY: y} = e, {offsetWidth: width, offsetHeight: height} = this, move = 25, xMove = x / width * (move * 2) - move, yMove = y / height * (move * 2) - move;
+                    span.style.transform = `translate(${xMove}px, ${yMove}px)`;
+                    circle.classList.add("hover");
+                    if (e.type === "mouseleave") {
+                        circle.classList.remove("hover");
+                        span.style.transform = "";
+                    }
+                };
+                window.addEventListener("mousemove", editPosCursor);
+                links.forEach((link => link.addEventListener("mousemove", animateit)));
+                links.forEach((link => link.addEventListener("mouseleave", animateit)));
+            }
+        }
+        const animLinks = document.querySelectorAll(".anim-link");
+        if (animLinks) {
+            const isPointerAvailableLink = window.matchMedia("(hover: hover)").matches;
+            if (isPointerAvailableLink) {
+                const animateit = function(e) {
+                    const span = this.querySelector(".anim-wrap");
+                    const {offsetX: x, offsetY: y} = e, {offsetWidth: width, offsetHeight: height} = this, move = 10, xMove = x / width * (move * 2) - move, yMove = y / height * (move * 2) - move;
+                    span.style.transform = `translate(${xMove}px, ${yMove}px)`;
+                    if (e.type === "mouseleave") span.style.transform = "";
+                };
+                animLinks.forEach((animLink => animLink.addEventListener("mousemove", animateit)));
+                animLinks.forEach((animLink => animLink.addEventListener("mouseleave", animateit)));
+            }
+        }
         window["FLS"] = false;
         menuInit();
         spollers();
